de25ff142498f36be03721669c4784ee21345d08
Milosz Lewandowski <milosz.lewandowski@icloud.com>
2023-11-01 18:04:03 +0100

    test 2

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 # Create a Chrome WebDriver instance
 driver = webdriver.Chrome()
 
+# test
 # Open a website in the Chrome window
 driver.get("https://www.example.com")
 
